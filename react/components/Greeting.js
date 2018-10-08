71661f6324bc0eeb664db78f4228f67a0ade64b2
Dan <daniel.ddsc@gmail.com>
2018-10-08 10:28:23 -0300

    Compose Greeting for simplicity

--- react/components/Greeting.js
+++ react/components/Greeting.js
@@ -1,34 +1,53 @@
 import React, { Fragment } from 'react'
-import { Query } from 'react-apollo'
+import PropTypes from 'prop-types'
+import { graphql } from 'react-apollo'
+import { compose, branch, withProps, renderComponent } from 'recompose'
 import { FormattedMessage } from 'react-intl'
 import GreetingLoading from './GreetingLoading'
-import getGreeting from '../graphql/getGreeting.gql'
+import { session } from 'vtex.store/Queries'
 
-const Greeting = () => (
-  <Query query={getGreeting} ssr={false}>
-    {({ data, error, loading }) =>
-      error ? null : (
-        <div className="vtex-page-padding mh7 pv6 f3 fw4 c-on-base">
-          {loading ? (
-            <div className="w-50-m">
-              <GreetingLoading />
-            </div>
-          ) : data.profile.firstName ? (
-            <Fragment>
-              <span className="pr2">
-                <FormattedMessage id="delivery-dreamstore.greeting" />,
-              </span>
-              <span className="fw6 nowrap">{data.profile.firstName}!</span>
-            </Fragment>
-          ) : (
-            <div className="nowrap">
-              <FormattedMessage id="delivery-dreamstore.greeting" />!
-            </div>
-          )}
-        </div>
-      )
-    }
-  </Query>
+const Greeting = ({ profile }) => {
+  if (!profile) return null
+
+  return Wrapper(
+    profile.firstName ? (
+      <Fragment>
+        <span className="pr2">
+          <FormattedMessage id="delivery-dreamstore.greeting" />,
+        </span>
+        <span className="fw6 nowrap">{data.profile.firstName}!</span>
+      </Fragment>
+    ) : (
+      <div className="nowrap">
+        <FormattedMessage id="delivery-dreamstore.greeting" />!
+      </div>
+    )
+  )
+}
+
+Greeting.defaultProps = {
+  profile: {},
+}
+
+Greeting.propTypes = {
+  profile: PropTypes.shape({
+    firstName: PropTypes.string,
+  }),
+}
+
+const Wrapper = Component => props => (
+  <div className="vtex-page-loading mh7 pv6 f3 fw4 c-on-base">
+    <Component {...props} />
+  </div>
 )
 
-export default Greeting
+const enhanced = compose(
+  graphql(session, { options: { ssr: false } }),
+  branch(
+    ({ data, loading }) => loading || data.getSession === null,
+    renderComponent(Wrapper(GreetingLoading)),
+    withProps(({ data }) => ({ profile: data.getSession.profile }))
+  )
+)
+
+export default enhanced(Greeting)
